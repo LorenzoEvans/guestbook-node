3e0ceef26921c142b501d9bc04306512c936b8fb
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-17 18:59:23 -0500

    Set up get endpoint, working on post currently.

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 var http = require("http")
 var path = require("path")
 var express = require("express")
+var logger = require("morgan")
+var bodyParser = require("body-parser")
+
+var app = express()
+
+app.set("views", path.resolve(__dirname, "views"))
+app.set("view engine", "ejs")
+
+var entries = []
+app.locals.entries = entries ;
+
+app.use(logger("dev"))
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get("/", (req, res) => {
+ Response.render("index")
+})
+
+app.get("/new-entry", (req, res) => {
+ Response.render("new-entry")
+})
+
+app.post("")
