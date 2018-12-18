7bb64a67d95194d5e14ee7baf0084fd152f9c422
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-17 19:10:24 -0500

    Set up get/post endpoint, able to run app now.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require("express")
 var logger = require("morgan")
 var bodyParser = require("body-parser")
 
+const PORT = 3030
+
 var app = express()
 
 app.set("views", path.resolve(__dirname, "views"))
@@ -17,11 +19,35 @@ app.use(logger("dev"))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
- Response.render("index")
+ res 
+  .render("index")
 })
 
 app.get("/new-entry", (req, res) => {
- Response.render("new-entry")
+ res
+  .render("new-entry")
 })
 
-app.post("")
+app.post("/new-entry", (req, res) => {
+ if (!req.body.title || !req.body.body) {
+  res
+   .status(400)
+   .send("Entries must have a title and a body")
+ }
+ entries.push({
+  title: req.body.title,
+  content: req.body.body,
+  published: new Date()
+ });
+ res.redirect("/")
+})
+
+app.use((req, res) => {
+ res
+  .status(404)
+  .render("404")
+})
+
+http.createServer(app).listen(PORT, () => {
+ console.log(`Guestbook running live on port ${PORT}`)
+})
